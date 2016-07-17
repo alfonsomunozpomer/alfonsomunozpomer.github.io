c91adf5a4b92ed65fed319c0fc7a03a2dbef3cea
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-07-17 19:22:17 +0100

    La falacia del futuro sin trabajo

---
+++ Pocket_ La falacia del futuro sin trabajo y de la revolución digital como causa del precariado_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "CAMBRIDGE",
+      'continent': "EU",
+      'country': "GB",
+      'region': "EN"
+    },
+    'ip':"82.17.135.126"
+  }]);
+})
+//
+()
+
+;
